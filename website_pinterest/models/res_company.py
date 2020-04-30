e04c3d2dd76f607b108ee957eeaa7f3361f31338
antoniofregoso <antonio.fregoso.mx@gmail.com>
2020-04-30 18:49:28 -0500

    [CHANGE] pinterest to UX

--- website_pinterest/models/res_company.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-
-class Company(models.Model):
-    _inherit = "res.company"
-
-    social_pinterest = fields.Char('Pinterest Account')
