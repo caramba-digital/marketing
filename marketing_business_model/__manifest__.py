430af74c100cb2577457a90a3c0e8221d721a8f7
antoniofregoso <yomero@antoniofregoso.com>
2020-08-14 17:12:14 -0500

    [ADD] module business model canvas

---
+++ marketing_business_model/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Marketing Business Model",
+
+    'summary': "Live Business Model Canvas",
+
+    'author': "Antonio Fregoso",
+    'website': "https://antoniofregoso.com",
+    'category': 'Marketing/Strategy',
+    'version': '13.0.0.2.0',
+    'license': 'AGPL-3',
+    'depends': ['marketing_strategy'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
