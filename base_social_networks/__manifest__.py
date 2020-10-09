cbc0c40761bd351003f9078ddf6c01340e8f2fd3
antoniofregoso <yomero@antoniofregoso.com>
2020-10-09 17:07:00 -0500

    [ADD] base_social_networks v 14

---
+++ base_social_networks/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# LICENSE GNU GENERAL PUBLIC LICENSE Version 3 
+
+{
+    'name': "Base Social Network",
+
+    'summary': "Profiles and pages on social networks",
+
+    'author': "Antonio Fregoso",
+    'website': "http://www.antoniofregoso.com",
+    'category': 'Marketing/Strategy',
+    'version': '0.1',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'views/partner_social_networks_views.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ base_social_networks/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# LICENSE GNU GENERAL PUBLIC LICENSE Version 3 
+
+from . import res_partner
